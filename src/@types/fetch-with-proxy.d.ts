b074116cab151083bcba9225789009967be85380
kako-jun <3541096+kako-jun@users.noreply.github.com>
2021-01-28 16:24:21 +0900

    change using request -> node-fetch-with-proxy.

---
+++ src/@types/fetch-with-proxy.d.ts
@@ -0,0 +1,1 @@
+declare module "fetch-with-proxy";
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,21 +1,27 @@
+import fetchWithProxy from "fetch-with-proxy";
+import * as chardet from "chardet";
 import * as iconv from "iconv-lite";
-import * as request from "request-promise-native";
-// tslint:disable-next-line no-var-requires no-require-imports
-const chardet = require("chardet");
 
-export const requestOptions: request.RequestPromiseOptions = {};
+// const sslRootCAs = require("ssl-root-cas/latest");
+// sslRootCAs.inject();
 
 export async function fetch(url: string) {
-    let responseBuffer;
-    try {
-        responseBuffer = await request(url, {...requestOptions, encoding: null});
-    } catch (error) {
-        const errorEncoding = chardet.detect(error.error);
-        const errorResponse = iconv.decode(error.error, errorEncoding);
-        throw new Error(`${error.message} ${errorResponse}`);
-    }
-    const encoding = chardet.detect(responseBuffer);
-    const response = iconv.decode(responseBuffer, encoding);
+  let buf: ArrayBuffer;
+  try {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+    const res: Response = await fetchWithProxy(url);
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "1";
+    buf = await res.arrayBuffer();
+  } catch (error) {
+    // do nothing.
+    throw error;
+  }
 
-    return response;
+  const encoding = chardet.detect(new Uint8Array(buf));
+  if (encoding) {
+    const html = iconv.decode(Buffer.from(buf), encoding);
+    return html;
+  }
+
+  return "";
 }
